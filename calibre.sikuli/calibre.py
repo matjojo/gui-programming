248b4fd338f65eb75b5951c3846b0c1c02746359
matjojo <mat2904jojo@gmail.com>
2019-03-23 18:38:08 +0100

    Adds functionality to press the view log button and exit after the last book has been imported and downloaded

--- calibre.sikuli/calibre.py
+++ calibre.sikuli/calibre.py
@@ -1,6 +1,7 @@
 import time
 adultSet = False
-while True:
+notDone = True
+while notDone:
     try:
         if find("1553270172366.png"):
          click(Pattern("1553270172366.png").targetOffset(110,27))
@@ -12,3 +13,10 @@ while True:
                 click(Pattern("1553360794403.png").targetOffset(126,15))
         except:
              pass
+    try:
+        if find("1553362571821.png"):
+            click(Pattern("1553362571821.png").targetOffset(100,13))
+            notDone = False # when we open the log we know this was the last button to press
+        except:
+            pass
+        
