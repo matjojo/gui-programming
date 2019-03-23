6bc3e27f2265b8be16ce89f0df7867e5da5c1745
matjojo <mat2904jojo@gmail.com>
2019-03-23 18:22:54 +0100

    Fixes wrong indent issue on new isAdult line

--- calibre.sikuli/calibre.py
+++ calibre.sikuli/calibre.py
@@ -6,7 +6,7 @@ while True:
          click(Pattern("1553270172366.png").targetOffset(110,27))
     except:
         time.sleep(5)
-   if not adultSet: # Make sure we do not search for this all the time when not needed
+    if not adultSet: # Make sure we do not search for this all the time when not needed
         try:
             if find("1553360794403.png"):
                 click(Pattern("1553360794403.png").targetOffset(126,15))
