6c2f387cfaca0c2e9fc559fcb601e730c434f24c
matjojo <mat2904jojo@gmail.com>
2019-03-23 18:21:42 +0100

    Adds boolean value and new image to search for the are you adult message and click it, it won't search after pressing yes once.

--- calibre.sikuli/calibre.py
+++ calibre.sikuli/calibre.py
@@ -1,7 +1,14 @@
 import time
+adultSet = False
 while True:
     try:
-        if find("1553112404444.png"):
-            click(Pattern("1553112404444.png").targetOffset(10,-1))
+        if find("1553270172366.png"):
+         click(Pattern("1553270172366.png").targetOffset(110,27))
     except:
         time.sleep(5)
+   if not adultSet: # Make sure we do not search for this all the time when not needed
+        try:
+            if find("1553360794403.png"):
+                click(Pattern("1553360794403.png").targetOffset(126,15))
+        except:
+             pass
